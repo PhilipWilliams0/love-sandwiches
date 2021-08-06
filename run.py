079c9610f05cedfc3325d5f32a397568173d212c
Philip Williams <williamsphilip@outlook.com>
2021-08-06 09:29:57 +0100

    Amended run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from logging import info
 from google.auth import credentials
 import  gspread
 from google.oauth2.service_account import Credentials
@@ -14,14 +15,16 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
+    """Get sales figures input from the user 
     """
-    Get sales figures input from the user 
-    """ 
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    print(sales_data)
 
 get_sales_data()
+
